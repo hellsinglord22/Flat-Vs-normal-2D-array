d6b4d5b4f21d03c93c0f2d0e7f0b047797dd84fe
H <heshamscorpe55@gmail.com>
2015-04-25 03:39:24 +0200

    Make attributes

--- src/App.java
+++ src/App.java
@@ -2,4 +2,18 @@
  * Description: The program shows the performance
  * advantage of 1D arrays over 2D arrays.
  * A flat array is nearly twice as fast.
- * */
+ */
+
+public class App {
+    public static void main(String[] args) {
+        // Attributes //
+        long startingTime, elapsedTime;
+
+        int[] flatArray = new int[10 * 10];
+        int[][] normal2DArray = new int[10][10];
+
+        flatArray[0 + (4 * 10)] = 1;
+        normal2DArray[0][4] = 1;
+
+    }
+}
